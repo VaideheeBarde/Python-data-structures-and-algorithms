115c6c7c2e38b0db4f8d761226598892072bcaf0
Vaidehee Barde <vaideheebarde914@gmail.com>
2017-11-11 12:33:07 -0800

    Add files via upload

---
+++ reverse a string.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 12:31:03 2017
+
+@author: Vaidehee
+"""
+
+#####Reverse a string without recursion
+
+#input Python
+#output nohtyP
+
+name = input ("Enter a string")
+print (name)
+print ("The reverse is")
+print (name[::-1])
