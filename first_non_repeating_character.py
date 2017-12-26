b06d068a6ba0b9909f08c5c574a4aa88f2440a65
Vaidehee Barde <vaideheebarde914@gmail.com>
2017-12-26 01:06:40 -0800

    Add files via upload

---
+++ first_non_repeating_character.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 25 13:32:56 2017
+
+@author: Vaidehee
+"""
+
+def firstnonrepeatingcharacter(a):
+    if(len(a)==len(b)):
+        print("_")
+    else:
+        for x in a:
+            if a.count(x)==1:
+                arr.append(x)
+                print(arr[0])
+                break
+        
+a=str(input("Enter a string"))
+a=list(a)
+b=set(a)
+arr=[]
+firstnonrepeatingcharacter(a)
