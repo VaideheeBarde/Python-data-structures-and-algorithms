c4ded8b2958c1bcae486316af4340ff27e6d3af7
Vaidehee Barde <vaideheebarde914@gmail.com>
2018-03-27 10:34:54 -0700

    Update given two arrays - find the missing element in the second array.py

--- given two arrays - find the missing element in the second array.py
+++ given two arrays - find the missing element in the second array.py
@@ -5,6 +5,13 @@ Created on Tue Mar 27 10:23:34 2018
 @author: Vaidehee
 """
 
+#we consider only non negative elements
+
+#input
+#finder([1,2,3,4,5,6,7], [2,3,4,5,6,1])
+
+#output
+#7
 
 def finder(arr1, arr2):
     
