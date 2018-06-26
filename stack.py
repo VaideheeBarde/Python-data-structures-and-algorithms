9ae93abce35ae0725aaf77953e613fb3ef99b927
Vaidehee Barde <vaideheebarde914@gmail.com>
2018-06-25 17:57:41 -0700

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 25 17:55:58 2018
+
+@author: Vaidehee
+"""
+
+#Implementation of a stack
+
+class Stack(object):
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def peek(self):
+        return self.items[len(self.items)-1]
+    def size(self):
+        return len(self.items)
